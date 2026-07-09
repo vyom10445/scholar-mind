bb9864d5134c4fd81c26ba14ca4f9bd76312cce7
vyom10445 <vyom.2427030058@muj.manipal.edu>
2026-07-10 01:56:13 +0530

    implement document retrieval from persisted Chroma vector store

--- main.py
+++ main.py
@@ -1,30 +1,79 @@
 from dotenv import load_dotenv
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_mistralai import ChatMistralAI
-from langchain_core.prompts import ChatPromptTemplate
-
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-
-
 load_dotenv()
-
-model = ChatMistralAI(model = "mistral-small-2506")
-
-loader = PyPDFLoader("document loaders/deeplearning.pdf")
-docs = loader.load()
-
-text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000 , chunk_overlap=200)
+from langchain_openai import ChatOpenAI , OpenAIEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.vectorstores import Chroma
 
 
+embedding = OpenAIEmbeddings()
 
-prompt_temp = ChatPromptTemplate([
-    ("system","you are an AI that summarizes text"),
-    ("human","{data}")
+#retreive data from chromadb
+vectorstore = Chroma(
+    persist_directory="chroma_db",
+    embedding_function=embedding
+)
+
+#create retrieval
+retriever = vectorstore.as_retriever(
+    search_type = 'mmr',
+    search_kwards = {
+        "k" : 4 ,
+        "fetch_k" : 10,
+        "lambda_mult" : 0.5
+    }
+)
+
+
+llm = ChatOpenAI(model="gpt-5.4-nano-2026-03-17")
+
+
+#prompt template 
+
+prompt = ChatPromptTemplate.from_messages([
+    (
+            "system",
+            """You are a helpful AI assistant.
+
+Use ONLY the provided context to answer the question.
+
+If the answer is not present in the context,
+say: "I could not find the answer in the document."
+"""
+        ),
+        (
+            "human",
+            """Context:
+{context}
+
+Question:
+{question}
+"""
+        )
 ])
 
-prompt = prompt_temp.invoke({"data": docs[0].page_content})
+#just for terminal purpose (would remove)
+
+print("RAG System created")
+
+print("press 0 to exit")
+
+while True:
+    query = input("You: ")
+    if query == "0":
+        break
+    
+    docs = retriever.invoke(query)
+
+    context = "\n\n".join(
+        [doc.page_content for doc in docs]
+    )
 
 
-result = model.invoke(prompt)
+    final_prompt = prompt.invoke({
+        "context" : context,
+        "question" : query
+    })
 
-print(result.content)
+    response = llm.invoke(final_prompt)
+
+    print(f"\n AI: {response.content}")
